50a0781259fe0361edc64de5345306b168688989
619Saurabh <160112390+619Saurabh@users.noreply.github.com>
2024-08-09 16:59:12 +0530

    Update CustomPagination.jsx

--- src/components/Pagination/CustomPagination.jsx
+++ src/components/Pagination/CustomPagination.jsx
@@ -37,13 +37,4 @@ const CustomPagination = ({ setPage, numberOfPages = 10 }) => {
 };
 
 export default CustomPagination;
-//We need to provide <Pagination/> component a few properties like onChange which will be occurred whenever we changed the page and which is going
-//to call a function called handlePageChange() which is responsible for updating the state page with that page no.
-//So, whenever we change the page no. it sets the page state to the particular page no. that we have clicked using setPage() setter function and it
-//should scroll back to the top of window
-//So, handlePageChange() function is responsible for updating our page no. Whenever the page state(which stores page no.) changes for eg. page no.
-//is 2 so its going to call the API again for page no. 2 to fetch the Movie or Tv series data for page 2.
 
-//count is the no. of pages in pagination
-//and numberOfPages is going to be the default value of 10 i.e By default, there is going to be 10 pages because mostly the API has like 500 to
-//600 pages data
